f218c1e4504e5627e77f4ecc42aec5b76c098f53
pmoreno.rodriguez <pmorenorodriguez@protonmail.com>
2024-11-09 10:43:11 +0100

    actualización css en tablas markdown

--- docs/assets/js/main.js
+++ docs/assets/js/main.js
@@ -1,47 +1,48 @@
 (function () {
-    // Función de configuración para extraer claves como :class=<nombre_clase>
-    function getAndRemoveConfig(str = '') {
-      const config = {};
-  
-      if (str) {
-        str = str
-          .replace(/^('|")/, '')
-          .replace(/('|")$/, '')
-          .replace(/(?:^|\s):([\w-]+:?)=?([\w-%]+)?/g, (m, key, value) => {
-            if (key.indexOf(':') === -1) {
-              config[key] = (value && value.replace(/&quot;/g, '')) || true;
-              return '';
-            }
-            return m;
-          })
-          .trim();
-      }
-  
-      return { str, config };
-    }
-  
-    var addHeadingClassesPlugin = function (hook, vm) {
-      hook.beforeEach(function (markdown) {
-        // Expresión regular que asegura un espacio antes de ":class="
-        const headingClassPattern = /^(#{1,6})\s*(.*?)\s+:class=([\w-]+)/gm;
-  
-        return markdown.replace(headingClassPattern, function (match, hashes, title, classConfig) {
-          const level = hashes.length; // Nivel de encabezado (#, ##, etc.)
-          
-          // Extrae el texto del título y la configuración
-          const { str: cleanTitle, config } = getAndRemoveConfig(`"${title} :class=${classConfig}"`);
-  
-          // Si existe configuración de clase, se añade al encabezado
-          if (config.class) {
-            return `<h${level} class="${config.class}">${cleanTitle}</h${level}>`;
-          } else {
-            return `<h${level}>${cleanTitle}</h${level}>`;
+  // Función de configuración para extraer claves como :class=<nombre_clase>
+  function getAndRemoveConfig(str = '') {
+    const config = {};
+
+    if (str) {
+      str = str
+        .replace(/^('|")/, '')
+        .replace(/('|")$/, '')
+        .replace(/(?:^|\s):([\w-]+:?)=?([\w-%]+)?/g, (m, key, value) => {
+          if (key.indexOf(':') === -1) {
+            config[key] = (value && value.replace(/&quot;/g, '')) || true;
+            return '';
           }
-        });
+          return m;
+        })
+        .trim();
+    }
+
+    return { str, config };
+  }
+
+  var addHeadingClassesPlugin = function (hook, vm) {
+    hook.beforeEach(function (markdown) {
+      // Expresión regular que asegura un espacio antes de ":class="
+      const headingClassPattern = /^(#{1,6})\s*(.*?)\s+:class=([\w-]+)/gm;
+
+      return markdown.replace(headingClassPattern, function (match, hashes, title, classConfig) {
+        const level = hashes.length; // Nivel de encabezado (#, ##, etc.)
+
+        // Extrae el texto del título y la configuración
+        const { str: cleanTitle, config } = getAndRemoveConfig(`"${title} :class=${classConfig}"`);
+
+        // Si existe configuración de clase, se añade al encabezado
+        if (config.class) {
+          return `<h${level} class="${config.class}">${cleanTitle}</h${level}>`;
+        } else {
+          return `<h${level}>${cleanTitle}</h${level}>`;
+        }
       });
-    };
-    
-    function addTableClasses() {
+    });
+  };
+
+  var addTableClasses = function (hook, vm) {
+    hook.doneEach(function () {
       var tables = document.querySelectorAll('table');
       tables.forEach(function(table) {
         var rows = table.querySelectorAll('tr');
@@ -56,11 +57,10 @@
           });
         });
       });
-    }
-    // Registro del plugin en Docsify
-    $docsify = $docsify || {};
-    $docsify.plugins = [].concat($docsify.plugins || [], addHeadingClassesPlugin, addTableClasses);
-  })();
-  
-  
-  
+    });
+  };
+
+  // Registro del plugin en Docsify
+  $docsify = $docsify || {};
+  $docsify.plugins = [].concat($docsify.plugins || [], addHeadingClassesPlugin, addTableClasses);
+})();
